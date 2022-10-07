b237f6d5a6beb554e006751eec78a67382b2f03d
Alejandro Ayala <alejandro.ayala@coppel.com>
2022-10-06 18:15:26 -0600

    se sube siiuu

--- src/js/index.js
+++ src/js/index.js
@@ -12,57 +12,83 @@ let contenedorPerfil = document.getElementById('contenedorPerfil')
 let contenedorPerfil1 = document.getElementById('contenedorPerfil1')
 const alertaEspacio = document.getElementById('liveAlertPlaceholder');
 
-if(btnBuscarPoke){
+if (btnBuscarPoke) {
     btnBuscarPoke.addEventListener('click', (e) => {
+        e.preventDefault();
         listaEstadisticas.innerHTML = '';
         listaTipos.innerHTML = '';
         listaHabilidades.innerHTML = '';
+        //Spinner
+        contenedorPerfil.style.display = 'none';
+        contenedorPerfil1.style.display = 'none';
+        var contenidoPadre = document.getElementById('contenido');
+        console.log(contenidoPadre);
+        const divSpinner = document.createElement('div');
+        divSpinner.classList.add('spinner');
+        contenidoPadre.appendChild(divSpinner);
+        setTimeout(() => {
+            //SPINNER
+            contenedorPerfil.style.display = 'block';
+            contenedorPerfil1.style.display = 'block';
+            divSpinner.remove();
+        }, 1000);
         getInfo(pokebusqueda.value.toLowerCase()).then((pokeinfo) => {
-             fillInfo(pokeinfo);
-        }).catch((error) =>{
+            fillInfo(pokeinfo);
+
+        }).catch((error) => {
             const wrapper = document.createElement('div')
-      wrapper.innerHTML = [
-        `<div class="alert alert-danger alert-dismissible" role="alert">`,
-        `   <div>El Pokémon no existe</div>`,
-        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-        '</div>'
-      ].join('')
-      alertaEspacio.append(wrapper);
+            wrapper.innerHTML = [
+                `<div class="alert alert-danger alert-dismissible" role="alert">`,
+                `   <div>El Pokémon no existe</div>`,
+                '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+                '</div>'
+            ].join('')
+            alertaEspacio.append(wrapper);
         });
-    
+
         pokebusqueda.value = '';
-        
+
     });
 }
 
-if(pokebusqueda){
+if (pokebusqueda) {
     pokebusqueda.addEventListener('keypress', (e) => {
-        if(e.key ==="Enter"){
+        if (e.key === "Enter") {
+            e.preventDefault();
             listaEstadisticas.innerHTML = '';
             listaTipos.innerHTML = '';
             listaHabilidades.innerHTML = '';
 
             //Spinner
+            contenedorPerfil.style.display = 'none';
+            contenedorPerfil1.style.display = 'none';
             var contenidoPadre = document.getElementById('contenido');
+            console.log(contenidoPadre);
             const divSpinner = document.createElement('div');
             divSpinner.classList.add('spinner');
-
-
+            contenidoPadre.appendChild(divSpinner);
+            setTimeout(() => {
+                //SPINNER
+                contenedorPerfil.style.display = 'block';
+                contenedorPerfil1.style.display = 'block';
+                divSpinner.remove();
+            }, 1000);
             getInfo(pokebusqueda.value.toLowerCase()).then((pokeinfo) => {
-            fillInfo(pokeinfo);
-        }).catch((error) =>{
-            const wrapper = document.createElement('div')
-      wrapper.innerHTML = [
-        `<div class="alert alert-danger alert-dismissible" role="alert">`,
-        `   <div>El Pokémon no existe</div>`,
-        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-        '</div>'
-      ].join('')
-      alertaEspacio.append(wrapper);
-        });
-    
-        pokebusqueda.value = '';
-        }        
+                fillInfo(pokeinfo);
+
+            }).catch((error) => {
+                const wrapper = document.createElement('div')
+                wrapper.innerHTML = [
+                    `<div class="alert alert-danger alert-dismissible" role="alert">`,
+                    `   <div>El Pokémon no existe</div>`,
+                    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+                    '</div>'
+                ].join('')
+                alertaEspacio.append(wrapper);
+            });
+
+            pokebusqueda.value = '';
+        }
     });
 }
 
@@ -73,8 +99,8 @@ getInfo(1).then((pokeinfo) => {
     fillInfo(pokeinfo);
 });
 //Obtener peliculas
-async function getInfo(id){
-    let url =` https://pokeapi.co/api/v2/pokemon/${id}/`;
+async function getInfo(id) {
+    let url = ` https://pokeapi.co/api/v2/pokemon/${id}/`;
 
     try {
         let response = await axios.get(url);
@@ -84,7 +110,7 @@ async function getInfo(id){
     }
 }
 
-function fillInfo(pokeinfo){
+function fillInfo(pokeinfo) {
     let tipoPoke;
     let habilidadPoke;
     let estadisticaPoke;
@@ -94,11 +120,11 @@ function fillInfo(pokeinfo){
     idpoke.innerHTML = pokeinfo.id;
     namepoke.innerHTML = pokeinfo.name;
     pesopoke.innerHTML = `${peso} KG`;
-    alturapoke.innerHTML =`${altura} M`;
+    alturapoke.innerHTML = `${altura} M`;
 
     const urlImage = pokeinfo.sprites.other['official-artwork']['front_default'];
     imagenpoke.src = urlImage;
-    imagenpoke.alt = "Pokemon: "+pokeinfo.name;
+    imagenpoke.alt = "Pokemon: " + pokeinfo.name;
 
     pokeinfo.types.forEach(tipo => {
         tipoPoke = document.createElement('li');
@@ -106,10 +132,10 @@ function fillInfo(pokeinfo){
         tipoPoke.id = tipo.type.name;
         tipoPoke.innerHTML = tipo.type.name;
         listaTipos.appendChild(tipoPoke);
-     });
+    });
 
     pokeinfo.abilities.forEach(habilidad => {
-       habilidadPoke = document.createElement('li');
+        habilidadPoke = document.createElement('li');
         //habilidadPoke.classList.add('nombrePoke');
         habilidadPoke.id = habilidad.ability.name;
         habilidadPoke.innerHTML = habilidad.ability.name;
@@ -118,11 +144,11 @@ function fillInfo(pokeinfo){
 
     pokeinfo.stats.forEach(estadistica => {
         estadisticaPoke = document.createElement('li');
-         //estadisticaPoke.classList.add('nombrePoke');
-         estadisticaPoke.id = estadistica["stat"]["name"];
-         estadisticaPoke.innerHTML =`${estadistica["stat"]["name"]} = ${estadistica["base_stat"]}`;
-         listaEstadisticas.appendChild(estadisticaPoke);
-     });
+        //estadisticaPoke.classList.add('nombrePoke');
+        estadisticaPoke.id = estadistica["stat"]["name"];
+        estadisticaPoke.innerHTML = `${estadistica["stat"]["name"]} = ${estadistica["base_stat"]}`;
+        listaEstadisticas.appendChild(estadisticaPoke);
+    });
 }
 
 //document.addEventListener("DOMContentLoaded", getInfo);
